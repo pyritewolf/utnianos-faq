1eb22d660f57f4b8945433ac1e30da8ca188f683
Jules Morelli <jmorelli@manas.tech>
2022-05-12 19:54:52 -0300

    Agregar footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,26 @@ import Inscripciones from './views/Inscripciones';
 import Finales from './views/Finales';
 import Votaciones from './views/Votaciones';
 import { PATHS } from './constants';
-import './App.css';
+import styles from './styles.module.css';
 
 function App() {
   return (
-    <div className="root container">
-      <header className="header">
+    <div className={styles.container}>
+      <header>
        <h1>UTNianos FAQ</h1>
       </header>
-      <Routes>
-        <Route path={PATHS.home} element={<Home />} />
-        <Route path={PATHS.lore} element={<Lore />} />
-        <Route path={PATHS.inscripciones} element={<Inscripciones />} />
-        <Route path={PATHS.finales} element={<Finales />} />
-        <Route path={PATHS.votaciones} element={<Votaciones />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path={PATHS.home} element={<Home />} />
+          <Route path={PATHS.lore} element={<Lore />} />
+          <Route path={PATHS.inscripciones} element={<Inscripciones />} />
+          <Route path={PATHS.finales} element={<Finales />} />
+          <Route path={PATHS.votaciones} element={<Votaciones />} />
+        </Routes>
+      </main>
+      <footer>
+        Hecho con el poder de los memes por UTNianos <a href="https://github.com/pyritewolf/utnianos-faq" target="_blank" rel="noreferrer"><i className="lab la-github"></i></a>
+      </footer>
     </div>
   );
 }
